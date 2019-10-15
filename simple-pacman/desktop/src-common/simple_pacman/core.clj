e3f0b0714b7367e7757592be1f47116df750ab6e
mey.beisaron <mey.beisaron@appsflyer.com>
2019-10-15 03:51:15 +0300

    small update to player map

--- simple-pacman/desktop/src-common/simple_pacman/core.clj
+++ simple-pacman/desktop/src-common/simple_pacman/core.clj
@@ -41,7 +41,7 @@
 
              (let [background (texture "background.png")
                    player (assoc (texture "pac.png")
-                            :x 40 :y 40 :width pac-size  :height pac-size  :player? true :direction :right)]
+                            :player? true :x 40 :y 40 :width pac-size  :height pac-size :angle 0  :direction :right)]
                [background player ]))
 
            :on-render
